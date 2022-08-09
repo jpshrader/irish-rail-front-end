6d9659e9e0700e39fc5f88ebdf07ca43a7e87bd3
Jack <96661942+jpshrader@users.noreply.github.com>
2022-08-09 12:32:59 -0400

    update jest to get tests passing

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,15 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { pathsToModuleNameMapper } = require('ts-jest');
 const { compilerOptions } = require('./tsconfig');
 
 module.exports = {
-  preset: 'jest-preset-angular',
-  roots: ['<rootDir>/src/'],
-  testMatch: ['**/+(*.)+(test).+(ts)'],
-  setupFilesAfterEnv: ['<rootDir>/src/test.ts'],
-  collectCoverage: true,
-  coverageReporters: ['html'],
-  coverageDirectory: 'coverage/my-app',
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
-    prefix: '<rootDir>/'
-  })
+	preset: 'jest-preset-angular',
+	roots: ['<rootDir>/src/'],
+	testMatch: ['**/+(*.)+(test).+(ts)'],
+	setupFilesAfterEnv: ['<rootDir>/src/test.ts'],
+	collectCoverage: true,
+	coverageReporters: ['html'],
+	coverageDirectory: 'coverage',
+	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+		prefix: '<rootDir>/'
+	})
 };
